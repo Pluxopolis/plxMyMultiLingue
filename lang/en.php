c3e09f6201056cdb22c3439ce36f8a726261cc95
Stephane <stephane@pluxml.org>
2014-05-12 15:33:22 +0200

    maj traduc fr/en

--- lang/en.php
+++ lang/en.php
@@ -1,13 +1,16 @@
 <?php
 
 $LANG = array(
-
 'L_SAVE'							=> 'Save',
 'L_FLAGS'							=> 'Select languages to manage',
 'L_LANG_IMAGES_FOLDER'				=> 'Use a different images folder for each langue',
 'L_LANG_DOCUMENTS_FOLDER'			=> 'Use a different documents folder for each langue',
+'L_FLAG'							=> 'Flag',
+'L_CAPTION'							=> 'Caption',
+'L_DISPLAY'							=> 'Type d\'affichage',
 'L_ORDER'							=> 'Order',
 'L_MISSING_CONFIG_PLUGIN'			=> 'Missing <strong>%s</strong> plugins configuration',
 'L_MISSING_CONFIG_PLUGINS'			=> 'Missing <strong>%s</strong> plugin configuration',
+'L_REDIRECT_IDENT'					=> 'Switch to the same ident when changing language for articles, categories or static pages',
 );
 ?>
--- lang/fr.php
+++ lang/fr.php
@@ -1,13 +1,16 @@
 <?php
 
 $LANG = array(
-
 'L_SAVE'							=> 'Enregister',
 'L_FLAGS'							=> 'Sélectionnez les langues à gérer',
 'L_LANG_IMAGES_FOLDER'				=> 'Utiliser un dossier images différent pour chaque langue',
 'L_LANG_DOCUMENTS_FOLDER'			=> 'Utiliser un dossier documents différent pour chaque langue',
+'L_DISPLAY'							=> 'Type d\'affichage',
+'L_FLAG'							=> 'Drapeau',
+'L_CAPTION'							=> 'Libellé',
 'L_ORDER'							=> 'Tri',
 'L_MISSING_CONFIG_PLUGINS'			=> 'Configuration <strong>%s</strong> absente pour les plugins',
 'L_MISSING_CONFIG_PLUGIN'			=> 'Configuration <strong>%s</strong> absente pour le plugin',
+'L_REDIRECT_IDENT'					=> 'Basculer sur le même identifiant lors du changement de langue pour les articles, catégories ou pages statiques',
 );
 ?>
