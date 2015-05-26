46812075b4db31abe2b3ce8be9c5621194282c30
Stephane <stephane@pluxml.org>
2015-05-26 15:09:11 +0200

    modif mise en page des pages d'aide

--- lang/en-help.php
+++ lang/en-help.php
@@ -1,15 +1,15 @@
 <p style="margin-top:20px">
 To display flags, edit sidebar.php in your theme folder and add this following line:
 <br /><br />
-<pre style="font-size:1.2em;margin-left:20px">
+<pre style="font-size:1.1em;margin-left:20px">
 &lt;?php eval($plxShow->callHook('MyMultiLingue')) ?>
 </pre>
 </p>
 <p style="margin-top:20px">
 <strong>Caution :<br />
 <ul>
-<li>- Set plxMyMultiLingue in first position in the active plugins list.</li>
-</li>- Activate url rewriting (Parameters > Advanced configuration, Enable url rewriting : Yes</li>
+<li>Set plxMyMultiLingue in first position in the active plugins list.</li>
+<li>Activate url rewriting (Parameters > Advanced configuration, Enable url rewriting : Yes</li>
 </ul>
 </strong>
 </p>
--- lang/fr-help.php
+++ lang/fr-help.php
@@ -1,15 +1,21 @@
 <p style="margin-top:20px">
 Pour afficher les drapeaux permettant de sélectionner une langue, éditez par exemple le fichier sidebar.php de votre thème et ajoutez la ligne suivante:
 <br /><br />
-<pre style="font-size:1.2em;margin-left:20px">
+<pre style="font-size:1.1em;margin-left:20px">
 &lt;?php eval($plxShow->callHook('MyMultiLingue')) ?>
 </pre>
 </p>
 <p style="margin-top:20px">
 <strong>Important :<br />
 <ul>
-<li>- plxMyMultiLingue doit être le premier plugin dans la liste des plugins actifs pour assurer un bon fonctionnement (notamment avec le plugin plxMyBetterUrl)</li>
-<li>- plxMyMultiLingue requiert l'activation de la réécriture d'url dans PluXml (Paramètres > Configuration avancée, Activer la réécriture d'urls : Oui</li>
+<li>
+	plxMyMultiLingue doit être le premier plugin dans la liste des plugins actifs pour assurer un bon fonctionnement<br />
+	Notamment avec le plugin plxMyBetterUrl.
+</li>
+<li>
+	plxMyMultiLingue requiert l'activation de la réécriture d'url dans PluXml<br />
+	Paramètres > Configuration avancée > Activer la réécriture d'urls : Oui
+</li>
 </ul>
 
 </strong>
